ded7849d64ae0ab206c0045f10d564e71e5f9e83
rraj1572 <91118622+rraj1572@users.noreply.github.com>
2021-12-24 18:43:20 +0530

    Add files via upload

---
+++ token.php
@@ -0,0 +1,42 @@
+<?php
+
+# © 2021 Avishkar Patil DO NOT EDIT ANYTHING TO KEEP RUNNING
+
+# Here I Put Token which Available Publicly I Recommended Use Your Own Token Here 
+# For Suppport @Avishkarpatil at Telegram or proavipatil@gmail.com
+
+include "ip.php";
+include "credentials.php";
+
+$jctBase = "cutibeau2ic";
+
+ 
+function tokformat($str)
+{
+$str= base64_encode(md5($str,true));
+return str_replace("\n","",str_replace("\r","",str_replace("/","_",str_replace("+","-",str_replace("=","",$str)))));
+}
+function generateJct($st, $pxe) 
+{
+ global $jctBase;
+ return trim(tokformat($jctBase . $st . $pxe));
+}
+function generatePxe() {
+return time() + 6000000;
+}
+function generateSt() {
+global $ssoToken;
+return tokformat($ssoToken);
+}
+function generateToken() {
+$st = generateSt();
+$pxe = generatePxe();
+$jct = generateJct($st, $pxe);
+return "?jct=" . $jct . "&pxe=" . $pxe . "&st=" . $st;
+}
+
+# © 2021 Avishkar Patil DO NOT EDIT ANYTHING TO KEEP RUNNING
+
+
+echo generateToken();
+?>
